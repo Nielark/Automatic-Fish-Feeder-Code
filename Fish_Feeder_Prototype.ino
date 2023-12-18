80fa56e69f88d67487f46704978cba8b78a72b23
Nielark <markdanielmagpantay2001@gmail.com>
2023-12-18 12:15:07 +0800

    first commit

---
+++ Fish_Feeder_Prototype.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+const int servoPin1 = 10;
+const int servoPin2 = 11;
+const int servoPin3 = 6;
+
+Servo servoM1;
+Servo servoM2;
+Servo servoM3;
+
+void setup()
+{
+  servoM1.attach(servoPin1);
+  servoM2.attach(servoPin2);
+  servoM3.attach(servoPin3);
+
+  // Set the initial state of servo motor into 0 degree
+  servoM1.write(0);
+  myServoM2.writeMicroseconds(1500);
+  servoM3.write(0);
+}
+
+void loop()
+{
+  // M1
+  // Drop feed from M1 to M2
+  servoM1.write(180);
+  delay(5000);
+  servoM1.write(0);
+  delay(3000);
+
+  // Drop feed from M2 to M3
+  servoM2.write(180);
+  delay(3000);
+
+  myServoM2.writeMicroseconds(1500);
+  
+  delay(10000);
+}
