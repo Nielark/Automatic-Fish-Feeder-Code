9e24aef1b6e1edc0c999a60813f80c104af7f2c9
Nielark <markdanielmagpantay2001@gmail.com>
2023-12-19 15:04:46 +0800

    added relay and ultrasonic sensor

--- Fish_Feeder_Prototype.ino
+++ Fish_Feeder_Prototype.ino
@@ -1,8 +1,18 @@
 #include <Servo.h>
+#include "ultrasonic.h"
 
-const int servoPin1 = 10;
-const int servoPin2 = 11;
-const int servoPin3 = 6;
+// Ultrasonic sensor variables
+const int  echoPin = 4;
+const int  triggerPin = 5;
+int inches = 0;
+int cm = 0;
+
+const int relayPin = 6;
+
+// Servo variables
+const int servoPin1 = 7;
+const int servoPin2 = 8;
+const int servoPin3 = 9;
 
 Servo servoM1;
 Servo servoM2;
@@ -10,6 +20,9 @@ Servo servoM3;
 
 void setup()
 {
+  // Set pins to output pins
+  pinMode(relayPin, OUTPUT);
+
   servoM1.attach(servoPin1);
   servoM2.attach(servoPin2);
   servoM3.attach(servoPin3);
@@ -18,10 +31,19 @@ void setup()
   servoM1.write(0);
   servoM2.write(0);
   servoM3.write(90);
+
+  digitalWrite(relayPin, LOW); // Set the initial state of relay to low
 }
 
 void loop()
 {
+  // ULTRASONIC SENSOR READING
+  // measure the ping time in cm
+  cm = 0.01723 * readUltrasonicDistance(triggerPin, echoPin);
+  
+  // convert to inches by dividing by 2.54
+  inches = (cm / 2.54);
+
   // M1
   // Drop feed from M1 to M2
   servoM1.write(180);
@@ -30,26 +52,28 @@ void loop()
   delay(3000);
 
   // TODO: Blower code at this part
+  digitalWrite(relayPin, HIGH); // Relay trigger to start the blower
 
   // Drop feed from M2 to M3
-  servoM2.write(180);   // Drop the feeds to M3
+  servoM2.write(180); // Drop the feeds to M3
   delay(3000);
 
-  servoM2.write(0);     // Return back to the initial position
+  servoM2.write(0); // Return back to the initial position
 
   // M3
   // Dispensed feed and swing left and right
-  for(int i = 0; i < 10; i++) {
-    servoM3.write(30); //Spin in one direction
-    delay(300);
+  for(int i = 0; i < 20; i++) {
+    servoM3.write(0); //Spin in one direction
+    delay(200);
     servoM3.write(90); // Stop
-    delay(500);
-    servoM3.write(150);  // Spin in opposite direction
-    delay(300);
-    servoM3.write(90);  // Stop
-    delay(500);
+    delay(230);
+    servoM3.write(180); // Spin in opposite direction
+    delay(200);
+    servoM3.write(90); // Stop
+    delay(230);
   }
   servoM3.write(90); // Stop
-  
+  digitalWrite(relayPin, LOW); // Relay trigger to stop the blower
+
   delay(10000); // Interval for the next feeding
 }
