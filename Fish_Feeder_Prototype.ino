be52b63cb2582a3cefdbc27ad3e9a736c61e2eee
Nielark <markdanielmagpantay2001@gmail.com>
2023-12-19 11:36:32 +0800

    m3

--- Fish_Feeder_Prototype.ino
+++ Fish_Feeder_Prototype.ino
@@ -16,7 +16,7 @@ void setup()
 
   // Set the initial state of servo motor into 0 degree
   servoM1.write(0);
-  servoM2.write(0);
+  servoM2.write(90);
   servoM3.write(90);
 }
 
@@ -29,12 +29,17 @@ void loop()
   servoM1.write(0);
   delay(3000);
 
+  // TODO: Blower code at this part
+
   // Drop feed from M2 to M3
-  // servoM2.write(180);
-  // delay(3000);
+  servoM2.write(0);   // Spin to drop the feeds to M3
+  delay(3000);
 
-  // servoM2.write(0);
+  servoM2.write(180); // Spin back to the initial position
+  servoM2.write(90);  // Stop
 
+  // M3
+  // Dispensed feed and swing left and right
   for(int i = 0; i < 10; i++) {
     servoM3.write(0); //Spin in one direction
     delay(300);
@@ -46,8 +51,6 @@ void loop()
     delay(500);
   }
   servoM3.write(90); // Stop
-
   
-  
-  delay(10000);
+  delay(10000); // Interval for the next feeding
 }
