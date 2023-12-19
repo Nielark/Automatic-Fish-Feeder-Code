3bb3dbc90ed2dc95ec77c57cfa016d8403641542
Nielark <markdanielmagpantay2001@gmail.com>
2023-12-19 16:16:07 +0800

    relay pin 13

--- Fish_Feeder_Prototype.ino
+++ Fish_Feeder_Prototype.ino
@@ -1,18 +1,18 @@
 #include <Servo.h>
+#include "ultrasonic.h"
 
-// Motor A connections
-const int enA = 9;
-const int in1 = 8;
-const int in2 = 7;
+// Ultrasonic sensor variables
+const int  echoPin = 4;
+const int  triggerPin = 5;
+int inches = 0;
+int cm = 0;
 
-// Motor B connections
-const int enB = 3;
-const int in3 = 5;
-const int in4 = 4;
+const int relayPin = 6;
 
-const int servoPin1 = 10;
-const int servoPin2 = 11;
-const int servoPin3 = 6;
+// Servo variables
+const int servoPin1 = 7;
+const int servoPin2 = 8;
+const int servoPin3 = 9;
 
 Servo servoM1;
 Servo servoM2;
@@ -20,19 +20,8 @@ Servo servoM3;
 
 void setup()
 {
-  // Set all the motor control pins to outputs
-	pinMode(enA, OUTPUT);
-	pinMode(enB, OUTPUT);
-	pinMode(in1, OUTPUT);
-	pinMode(in2, OUTPUT);
-	pinMode(in3, OUTPUT);
-	pinMode(in4, OUTPUT);
-	pinMode(13, OUTPUT);
-	// Turn off motors - Initial state
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-	digitalWrite(in3, LOW);
-	digitalWrite(in4, LOW);
+  // Set pins to output pins
+  pinMode(13, OUTPUT);
 
   servoM1.attach(servoPin1);
   servoM2.attach(servoPin2);
@@ -41,57 +30,48 @@ void setup()
   // Set the initial state of servo motor into 0 degree
   servoM1.write(0);
   servoM2.write(0);
-  servoM3.write(0);
+  servoM3.write(90);
 }
 
 void loop()
 {
+  // ULTRASONIC SENSOR READING
+  // measure the ping time in cm
+  cm = 0.01723 * readUltrasonicDistance(triggerPin, echoPin);
+  
+  // convert to inches by dividing by 2.54
+  inches = (cm / 2.54);
+
   // M1
   // Drop feed from M1 to M2
   servoM1.write(180);
   delay(5000);
   servoM1.write(0);
   delay(3000);
-  
-  // M2 AND M3
-  // Set motors to maximum speed
-	// For PWM maximum possible values are 0 to 255
-  digitalWrite(13, HIGH);
-	analogWrite(enA, 255);    // Blower speed
-  analogWrite(enB, 255);    // Gearbox Motor speed
 
-  // Blower rotate in clockwise direction 
-  digitalWrite(in1, HIGH);
-	digitalWrite(in2, LOW);
+  // TODO: Blower code at this part
+  digitalWrite(13, HIGH); // Relay trigger to start the blower
 
   // Drop feed from M2 to M3
-  servoM2.write(180);
+  servoM2.write(180); // Drop the feeds to M3
+  delay(3000);
 
-  // Gear Box motor swing left and right direction
-  for(int i = 0; i < 10; i++){
-    digitalWrite(in3, HIGH);
-	  digitalWrite(in4, LOW);
-    delay(1000);
-    digitalWrite(in3, LOW);
-	  digitalWrite(in4, HIGH);
-    delay(1000);
+  servoM2.write(0); // Return back to the initial position
+
+  // M3
+  // Dispensed feed and swing left and right
+  for(int i = 0; i < 20; i++) {
+    servoM3.write(0); //Spin in one direction
+    delay(200);
+    servoM3.write(90); // Stop
+    delay(230);
+    servoM3.write(180); // Spin in opposite direction
+    delay(200);
+    servoM3.write(90); // Stop
+    delay(230);
   }
+  servoM3.write(90); // Stop
+  digitalWrite(13, LOW); // Relay trigger to stop the blower
 
-  // Servo motor swing left and right
-  // for(int i = 0; i < 10; i++){
-  //   servoM3.write(30);
-  //   delay(1000);
-  //   servoM3.write(0);
-  //   delay(1000);
-  // }
-  servoM2.write(0);  
-
-  // Turn off motors
-  digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-	digitalWrite(in4, LOW);
-  digitalWrite(13, LOW);
-  
-  delay(10000);
+  delay(10000); // Interval for the next feeding
 }
